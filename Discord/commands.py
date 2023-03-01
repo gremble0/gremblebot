5896b73aab70d6d1d95ab96abc80f38e3ca451d9
gremble0 <hermanstornes@gmail.com>
2023-03-01 14:00:38 +0100

    Made Playlist class, cont play command

--- Discord/commands.py
+++ Discord/commands.py
@@ -1,11 +1,8 @@
 import random
 import discord
-import secrets
 from pytube import YouTube, Search
-from googleapiclient.discovery import build
 
-async def handle_command(client, message):
-	#voice_client = discord.VoiceClient(client, message.channel)
+async def handle_command(client, message, playlist):
 	message_split = message.content.split()
 
 	if message_split[0] == "!ping":
@@ -14,8 +11,13 @@ async def handle_command(client, message):
 	if message_split[0] == "!play":
 		if len(message_split) < 2:
 			return "Please enter a video title"
+		if len(playlist.queue) >= 10:
+			return "Queue full"
 
-		yt = Search(" ".join(message_split[1:])).results
-		yt[0].streams.get_audio_only().download(output_path="media/")
+		yt = Search(" ".join(message_split[1:])).results[0] # returns first YouTube object in SearchQuery object
+		playlist.insert(yt)
 
-		return "asd"
+		await message.author.voice.channel.connect()
+		message.channel.send(playlist.play())
+
+		return f"Added {yt.title} to the queue."
---
+++ Discord/playlist.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+import discord
+import os
+
+class Playlist:
+	def __init__(self, client):
+		self.queue = []
+		self.client = client
+
+	def pop(self):
+		self.queue.pop(0)
+	
+	def insert(self, yt):
+		self.queue.append(yt)
+
+	def play(self):
+		yt_obj = self.queue[0]
+		stream = yt_obj.streams.get_lowest_resolution()
+		video_title = stream.download(output_path="media/")
+		#os.rename(video_title, video_title + ".mp3")
+		
+		return discord.file(open(video_title, "rb"))
+		#voice_client = discord.VoiceChannel.connect()
+		#discord.VoiceChannel.send(file=discord.File(open(video_title + ".mp3", "rb")))
